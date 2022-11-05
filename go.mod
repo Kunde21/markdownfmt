0ec52a02a1388f9cdea257374f207a1c6640d94d
Abhinav Gupta <mail@abhinavg.net>
2022-11-05 09:16:42 -0700

    main: Don't shell out to 'diff'
    Follow up to #40 that makes a similar change
    to the markdownfmt executable.
    
    Instead of shelling out to the 'diff' program,
    run diff inside the binary with [pkg/diff],
    using filenames 'a/$file' and 'b/$file'.
    
      [pkg/diff]: https://pkg.go.dev/github.com/pkg/diff
    
    Secondly, print the `diff $file markdownfmt/$file`
    message to stderr instead of stdout.
    I'm also okay with deleting this print statement completely,
    but I retained it to match the old behavior.
    
    Combined, this does two things:
    
    - The markdownfmt binary is no longer system-dependent
      and will work on Windows,
      and any other environment where 'diff' isn't available.
    - The output of `markdownfmt -d` can now be fed directly
      into `git apply`.

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/Kunde21/markdownfmt/v2
 require (
 	github.com/google/go-cmp v0.5.9
 	github.com/mattn/go-runewidth v0.0.9
+	github.com/pkg/diff v0.0.0-20210226163009-20ebb0f2a09e
 	github.com/pkg/errors v0.9.1
 	github.com/yuin/goldmark v1.3.5
 )
--- main.go
+++ main.go
@@ -8,12 +8,12 @@ import (
 	"go/scanner"
 	"io"
 	"os"
-	"os/exec"
 	"path/filepath"
 	"strings"
 
 	"github.com/Kunde21/markdownfmt/v2/markdown"
 	"github.com/Kunde21/markdownfmt/v2/markdownfmt"
+	"github.com/pkg/diff"
 )
 
 var (
@@ -82,12 +82,12 @@ func processFile(filename string, in io.Reader, out io.Writer) error {
 			}
 		}
 		if *doDiff {
-			data, err := diff(src, res)
-			if err != nil {
-				return fmt.Errorf("computing diff: %s", err)
-			}
-			fmt.Printf("diff %s markdownfmt/%s\n", filename, filename)
-			_, err = out.Write(data)
+			fmt.Fprintf(os.Stderr, "diff %s markdownfmt/%s\n", filename, filename)
+			err = diff.Text(
+				filepath.Join("a", filename),
+				filepath.Join("b", filename),
+				src, res, out,
+			)
 			if err != nil {
 				return fmt.Errorf("writing out: %s", err)
 			}
@@ -149,38 +149,4 @@ func markdownfmtMain() {
 			}
 		}
 	}
-}
-
-func diff(b1, b2 []byte) (data []byte, err error) {
-	f1, err := os.CreateTemp("", "markdownfmt")
-	if err != nil {
-		return
-	}
-	defer os.Remove(f1.Name())
-	defer f1.Close()
-
-	f2, err := os.CreateTemp("", "markdownfmt")
-	if err != nil {
-		return
-	}
-	defer os.Remove(f2.Name())
-	defer f2.Close()
-
-	_, err = f1.Write(b1)
-	if err != nil {
-		return
-	}
-
-	_, err = f2.Write(b2)
-	if err != nil {
-		return
-	}
-
-	data, err = exec.Command("diff", "-u", f1.Name(), f2.Name()).CombinedOutput()
-	if len(data) > 0 {
-		// diff exits with a non-zero status when the files don't match.
-		// Ignore that failure as long as we get output.
-		err = nil
-	}
-	return
 }
